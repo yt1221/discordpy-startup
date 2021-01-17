a67441d459305d837cd3ad3dc713ad5fa2e47de6
yt1221 <48877561+yt1221@users.noreply.github.com>
2021-01-17 20:35:38 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,88 @@
-from discord.ext import commands
 import os
-import traceback
+import discord
+from discord.ext import commands
 
-bot = commands.Bot(command_prefix='/')
+#client = discord.Client()
 token = os.environ['DISCORD_BOT_TOKEN']
+bot = commands.Bot(command_prefix='!')
+muteList = []
+channel_mem = []
+
+@bot.event
+async def on_ready():
+    global muteList
+    global channel_mem
+
+    print('-----Logged in info-----')
+    print(bot.user.name)
+    print(bot.user.id)
+    print(discord.__version__)
+    print('------------------------')
+    muteList = []
+    channel_mem = []
+
+@bot.command()
+async def voicekickall(ctx):
+    state = ctx.author.voice
+
+    for member in state.channel.members:
+
+        await member.move_to(None)
+
+@bot.command()
+async def vka(ctx):
+    state = ctx.author.voice
+
+    for member in state.channel.members:
+
+        await member.move_to(None)
+
+@bot.command()
+async def voicekickmention(ctx):
+    mentions = ctx.message.mentions
+
+    for member in mentions:
+
+        await member.move_to(None)
+
+@bot.command()
+async def vkm(ctx):
+    mentions = ctx.message.mentions
+
+    for member in mentions:
+
+        await member.move_to(None)
+
+@bot.command()
+async def muteadd(ctx):
+    global muteList
+    global channel_mem
+
+    muteList = ctx.message.mentions
+    channel_mem = [i.id for i in muteList]
+
+@bot.command()
+async def mutereset(ctx):
+    global muteList
+    global channel_mem
+
+    muteList = []
+    channel_mem = []
 
 
 @bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+async def on_voice_state_update(member,before,after):
+    global muteList
+    global channel_mem
 
+    if len(channel_mem) == 0:
+        return
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+    if member.id in channel_mem:
 
+        if not after.mute:
+            for member in muteList:
 
-bot.run(token)
+                await member.edit(mute=True)
+
+bot.run(TOKEN)
